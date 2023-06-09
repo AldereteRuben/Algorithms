ae0ad22cf8586af39bf34b83525301d391100cc3
Ruben Alderete <ruben.alderete@solera.com>
2023-06-09 10:16:07 +0200

    binarySearch implemented

---
+++ searching/binarySearch.js
@@ -0,0 +1,27 @@
+/**
+ * Performs a binary search on a sorted array to determine if a value is present or not.
+ *
+ * @param {Array} array - The sorted array to perform the search on.
+ * @param {*} value - The value to search for in the array.
+ * @returns {boolean} - Returns true if the value is present in the array, or false otherwise.
+ */
+const binarySearch = (array, value) => {
+    let left = 0;
+    let right = array.length;
+
+    while (left <= right) {
+        let middle = Math.floor((left + right) / 2);
+        let middleElement = array[middle];
+        if (middleElement === value) {
+            return true;
+        } else if (value < middleElement) {
+            right = middle - 1;
+        } else {
+            left = middle + 1;
+        }
+    }
+    return false;
+}
+
+const array = [1, 2, 3, 4, 5, 6];
+console.log(binarySearch(array, 7));
